19a8075ed96dff4af88f97792f43d388ed2cfd96
范勤阳 <qinyang.fan@xsuperzone.com>
2021-12-01 15:24:50 +0800

    自己实现ajax

--- main.js
+++ main.js
@@ -5,19 +5,32 @@ window.JQuery = function (nodeorSelector) {
 }
 
 window.JQuery.ajax = function (options) {
-    let url = options.url;
+    let url;
+
+    // 添加用户可以任意设置参数个数的功能
+    // arguments 实参
+    if (arguments.length === 1) {
+        url = options.url;
+    } else if (arguments.length === 2) {
+        url = arguments[0];
+        options = arguments[1];
+    }
+
     let method = options.method;
     let body = options.body;
     let successFn = options.successFn;
     let failFn = options.failFn;
     let headers = options.headers;
-    
+
+    // ES6 解构赋值
+    let { method, body, successFn, failFn, headers } = options;
+
     var request = new XMLHttpRequest();
     request.open(method, url);
     // 设置请求头需要在 open 方法后面
-    for(let key in headers){
+    for (let key in headers) {
         let value = headers[key];
-        request.setRequestHeader(key,value);
+        request.setRequestHeader(key, value);
     }
     request.send(body);
     request.onreadystatechange = () => {
@@ -39,16 +52,20 @@ let f2 = function (responseText) { console.log(responseText) }
 button.addEventListener("click", () => {
     window.JQuery.ajax(
         {
+            // 请求的第一部分
             url: "/xxx",
             method: "post",
-            body: "",
-            headers:{
-                "content-type":"text/html",
-                "fan":"18"
+            // 请求的第四部分
+            body: "123",
+            // 请求的第三部分
+            headers: {
+                "content-type": "text/html",
+                "fan": "18",
             },
             successFn: (x) => {
+                // 请求成功时，我想执行俩个函数
                 f1.call(undefined, x);
-                f2.call(undefined,x);
+                f2.call(undefined, x);
             },
             failFn: (x) => { console.log(x) }
         }
