a8d8d4627a0e09948b791f3d0d439cabd1f46668
范勤阳 <qinyang.fan@xsuperzone.com>
2021-12-01 16:20:09 +0800

    Ajax封装的Promise的使用

--- main.js
+++ main.js
@@ -1,73 +1,17 @@
-window.JQuery = function (nodeorSelector) {
-    nodes.addClass = function () { };
-    nodes.html = function () { };
-    return nodes;
-}
-
-window.JQuery.ajax = function (options) {
-    let url;
-
-    // 添加用户可以任意设置参数个数的功能
-    // arguments 实参
-    if (arguments.length === 1) {
-        url = options.url;
-    } else if (arguments.length === 2) {
-        url = arguments[0];
-        options = arguments[1];
-    }
-
-    let method = options.method;
-    let body = options.body;
-    let successFn = options.successFn;
-    let failFn = options.failFn;
-    let headers = options.headers;
-
-    // ES6 解构赋值
-    let { method, body, successFn, failFn, headers } = options;
-
-    var request = new XMLHttpRequest();
-    request.open(method, url);
-    // 设置请求头需要在 open 方法后面
-    for (let key in headers) {
-        let value = headers[key];
-        request.setRequestHeader(key, value);
-    }
-    request.send(body);
-    request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                successFn.call(undefined, request.responseText)
-            } else if (request.status >= 400) {
-                failFn.call(undefined, request)
-            }
-        }
-    }
-}
-
-window.$ = window.JQuery;
-
-let f1 = function (responseText) { console.log(responseText) }
-let f2 = function (responseText) { console.log(responseText) }
-
 button.addEventListener("click", () => {
-    window.JQuery.ajax(
+    $.ajax(
         {
-            // 请求的第一部分
-            url: "/xxx",
-            method: "post",
-            // 请求的第四部分
-            body: "123",
-            // 请求的第三部分
-            headers: {
-                "content-type": "text/html",
-                "fan": "18",
-            },
-            successFn: (x) => {
-                // 请求成功时，我想执行俩个函数
-                f1.call(undefined, x);
-                f2.call(undefined, x);
-            },
-            failFn: (x) => { console.log(x) }
+            url: "/xxxsadf",
+            method: "POST",
         }
-    )
+    ).then(
+        (sss) => { console.log(sss); return "第一次成功的返回值"; },
+        (qqq) => { console.log(qqq); return "第一次失败的返回值" })
+        // 第一次 then 成功时，默认的传过去的实参是 response
+        // 第一次 then 失败时，默认的传过去的实参是 request
+     .then(
+        (sdfa) => { console.log(sdfa) },
+        (sadfsadf) => { console.log(sadfsadf) })
+        // 后面 then 成功时，默认的传过去的实参是 第一次 then 成功时的返回值，如果没有返回值就手写 return，否则为 undefined
+        // 后面 then 失败时，默认的传过去的实参是 第一次 then 失败时的返回值，如果没有返回值就手写 return，否则为 undefined
 })
